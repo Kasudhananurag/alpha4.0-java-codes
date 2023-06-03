996b8c4b9c4c0e462a51844b9bca11f4a836f8a8
Anurag Kasudhan <114092146+Kasudhananurag@users.noreply.github.com>
2023-06-03 23:44:22 +0530

    creating and calling simoe function
    function inside a class become methods of that class

---
+++ func1.java
@@ -0,0 +1,8 @@
+public class func1 {
+    public static void printMsg(){
+        System.out.println("hello world");
+    }
+    public static void main(String[] args) {
+        printMsg();
+    }
+}
