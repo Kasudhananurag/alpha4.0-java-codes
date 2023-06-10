2fa916fe33e773450a5d5314e7b12a33a34f4fb3
Anurag Kasudhan <114092146+Kasudhananurag@users.noreply.github.com>
2023-06-10 13:08:27 +0530

    Add files via upload

--- patterns.java
+++ patterns.java
@@ -63,21 +63,71 @@ public class patterns {
         }
     }
 
+    public static void zero_one_triangle(int row){
+        int count = 1;
+        for(int i = 1; i<= row; i++){
+            for(int j = 1;j<=i;j++){
+            if((i+j)%2==0){
+                System.out.print("1");
+            }
+            else{
+                System.out.print("0");
+            }
+        }
+        System.out.println();
+        }
+    }
+
+    public static void butterfly(int row){
+        for(int i = 1;i<=row;i++){
+                //stars - i
+                for(int j = 1;j<=i;j++){
+                    System.out.print("*");
+                }
+                //spaces - 2*(row-i)
+                for(int j = 1;j<=2*(row-i);j++){
+                    System.out.print(" ");
+                }
+                //stars - i
+                for(int j = 1;j<=i;j++){
+                    System.out.print("*");
+                }
+                System.out.println();
+        }
+        for(int i = row;i>=1;i--){
+            //stars - i
+            for(int j = 1;j<=i;j++){
+                System.out.print("*");
+            }
+            //spaces - 2*(row-i)
+            for(int j = 1;j<=2*(row-i);j++){
+                System.out.print(" ");
+            }
+            //stars - i
+            for(int j = 1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+    }
+    }
+
     public static void main(String[] args) {
         int row,col;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of rows and columns");
         row = sc.nextInt();
         col = sc.nextInt();
-        hollow_Rectangle(row,col);
-        System.out.println();
-        invertedRotatedPyramid(row);
-        System.out.println();
-        pyramidStars(row);
-        System.out.println();
-        invertedHalfPyramidNumbers(row);
-        System.out.println();
-        floydTriangle(row);
+        // hollow_Rectangle(row,col);
+        // System.out.println();
+        // invertedRotatedPyramid(row);
+        // System.out.println();
+        // pyramidStars(row);
+        // System.out.println();
+        // invertedHalfPyramidNumbers(row);
+        // System.out.println();
+        // floydTriangle(row);
+        // zero_one_triangle(row);
+        butterfly(row);
     }
 
 }
